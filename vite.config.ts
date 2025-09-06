965ebfbf32610ba69a80367dd98fbc6517b95d40
frontend-en <kristinarwebdev@gmail.com>
2025-09-06 13:29:10 +0300

    Добавил конфиг для GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react()],
+   base: "/VOXMASTER/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"), // нормальный алиас на src
